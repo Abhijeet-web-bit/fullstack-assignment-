0810aceaaf2bd9592c6670bda5b2b95ce223e6fa
Abhijeet-web-bit <154255230+Abhijeet-web-bit@users.noreply.github.com>
2026-09-18 10:23:51 +0530

    Create taskRoutes.js

---
+++ routes/taskRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { authMiddleware } = require('../middleware/auth');
+const {
+  getTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask
+} = require('../controllers/taskController');
+
+router.use(authMiddleware);
+
+router.get('/', getTasks);
+router.get('/:id', getTaskById);
+router.post('/', createTask);
+router.put('/:id', updateTask);
+router.delete('/:id', deleteTask);
+
+module.exports = router;
